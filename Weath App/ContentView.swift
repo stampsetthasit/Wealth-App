7f5f3dc7021ff22459ae7ba23c9726ddcf2217a2
yadar <penphatulaya@icloud.com>
2023-03-02 08:26:45 +0700

    Initial Commit

---
+++ Weath App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Weath App
+//
+//  Created by Yadar Tulayathamrong on 2/3/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weath App/Weath_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weath_AppApp.swift
+//  Weath App
+//
+//  Created by Yadar Tulayathamrong on 2/3/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Weath_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
