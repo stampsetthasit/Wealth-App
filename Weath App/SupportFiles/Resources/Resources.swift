c74bc5b99d58de183be5353501020b275e4510ad
Setthasit Poosawat <setthasit.developer@gmail.com>
2023-03-10 18:09:47 +0700

    Add enum Resources

---
+++ Weath App/SupportFiles/Resources/Resources.swift
@@ -0,0 +1,101 @@
+//
+//  Resources.swift
+//  Weath App
+//
+//  Created by Setthasit Poosawat on 10/3/23.
+//
+
+import SwiftUI
+
+enum Resources {
+    enum Images {
+        
+        //MARK: - Weather
+        
+        enum Weather {
+            static var sun: UIImage {
+                UIImage(systemName: "sun.max.fill")!
+            }
+            static var moon: UIImage {
+                UIImage(systemName: "moon.fill")!
+            }
+            
+            static var cloudSun: UIImage {
+                UIImage(systemName: "cloud.sun.fill")!
+            }
+            
+            static var cloudMoon: UIImage {
+                UIImage(systemName: "cloud.moon.fill")!
+            }
+            
+            static var cloud: UIImage {
+                UIImage(systemName: "cloud.fill")!
+            }
+            
+            static var fog: UIImage {
+                UIImage(systemName: "cloud.fog.fill")!
+            }
+            
+            static var cloudSunRain: UIImage {
+                UIImage(systemName: "cloud.sun.rain.fill")!
+            }
+            
+            static var cloudMoonRain: UIImage {
+                UIImage(systemName: "cloud.moon.rain.fill")!
+            }
+            
+            static var cloudSnow: UIImage {
+                UIImage(systemName: "cloud.snow.fill")!
+            }
+            
+            static var cloudSleet: UIImage {
+                UIImage(systemName: "cloud.sleet.fill")!
+            }
+            
+            static var cloudDrizzle: UIImage {
+                UIImage(systemName: "cloud.drizzle.fill")!
+            }
+            
+            static var cloudSunBolt: UIImage {
+                UIImage(systemName: "cloud.sun.bolt.fill")!
+            }
+            
+            static var cloudMoonBolt: UIImage {
+                UIImage(systemName: "cloud.moon.bolt.fill")!
+            }
+            
+            static var windSnow: UIImage {
+                UIImage(systemName: "wind.snow")!
+            }
+            
+            static var cloudHeavyRain: UIImage {
+                UIImage(systemName: "cloud.heavyrain.fill")!
+            }
+            
+            static var snowflake: UIImage {
+                UIImage(systemName: "snowflake")!
+            }
+            
+            static var cloudHail: UIImage {
+                UIImage(systemName: "cloud.hail.fill")!
+            }
+            
+            static var cloudBoldRain: UIImage {
+                UIImage(systemName: "cloud.bolt.rain.fill")!
+            }
+            
+            static var wind: UIImage {
+                UIImage(systemName: "wind")!
+            }
+            
+            static var drop: UIImage {
+                UIImage(systemName: "drop.fill")!
+            }
+            
+            static var dropDegreesign: UIImage {
+                UIImage(systemName: "drop.degreesign.fill")!
+            }
+            
+        }
+    }
+}
